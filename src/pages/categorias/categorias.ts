9a1814edf8e04ad911250517cd2e7f4ecdbbee3e
Adilson Batista <adbsilvajr@gmail.com>
2019-06-06 21:36:59 -0300

    listando categorias utilizando o componente Thumbnail List

--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { CategoriaService } from '../../services/domain/categoria.service';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { API_CONFIG } from '../../config/api.config';
 
 /**
  * Generated class for the CategoriasPage page.
@@ -16,18 +18,21 @@ import { CategoriaService } from '../../services/domain/categoria.service';
 })
 export class CategoriasPage {
 
+imgsUrl: string = API_CONFIG.imgsUrl; 
+items: CategoriaDTO[];
+
   constructor(
     public navCtrl: NavController, 
     public navParams: NavParams,
     public categoriaService: CategoriaService
     ) {
-  
+ 
     }
 
   ionViewDidLoad() {
     this.categoriaService.findAll()
         .subscribe(response => {
-          console.log(response);
+          this.items = response;
         },
         error => {
           console.log(error)
